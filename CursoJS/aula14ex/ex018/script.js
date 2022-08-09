cfe885868b8c6334626c0285034eb6c05478c3d0
Valdez Junior <valdez.moral@gmail.com>
2022-08-09 20:14:01 -0300

    :package: finalizado

--- CursoJS/aula14ex/ex018/script.js
+++ CursoJS/aula14ex/ex018/script.js
@@ -1,17 +1,17 @@
 function tabuada() {
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
+    let num = document.getElementById('txtn');
+    let tab = document.getElementById('seltab');
 
     if (num.value.length == 0) {
-        window.alert('Por favor, digite um número!')
+        alert('Por favor, digite um número!');
     } else {
         var n = Number(num.value)
-        tab.innerHTML = ''
+        tab.innerHTML = '';
         for( c = 1; c <= 10; c++) {
-        var item = document.createElement('option')
+        var item = document.createElement('option');
         
         item.text = `O valor é = ${n*c}`
-        tab.appendChild(item)
+        tab.appendChild(item);
     }
     }
 }
